c704952e86a0390d22ca35e84797fba10421c2f2
Rishi Kothari <itsrishikothari@gmail.com>
2020-06-12 18:32:50 -0400

    main

---
+++ Hello World/src/main.cpp
@@ -0,0 +1,34 @@
+/**
+ * Blink
+ *
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+#include "Arduino.h"
+
+// Set LED_BUILTIN if it is not defined by Arduino framework
+// #define LED_BUILTIN 13
+#define BUTTON 44
+
+void setup()
+{
+  Serial.begin(9600);
+  // initialize LED digital pin as an output.
+  pinMode(BUTTON, INPUT);
+}
+
+int state = 0;
+
+void loop()
+{
+
+  int buttonState = digitalRead(BUTTON);
+
+  if (buttonState == HIGH) {
+    ++state;
+  } 
+
+  Serial.println(state);
+
+  delay(1);
+}
